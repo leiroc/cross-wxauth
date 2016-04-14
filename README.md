03e10db953b1e7a4a4b5db04693f834a3a6f74b7
wbh5 <121644750@qq.com>
2016-04-14 12:16:28 +0800

    cross-wxauth

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # cross-wxauth
+
 微信获取用户信息，接口提供给没有服务号的用户使用，或者个人使用；
+
+##官方微信授权步骤
+- 第一步：用户同意授权，获取code
+
+- 第二步：通过code换取网页授权access_token
+
+- 第三步：刷新access_token（如果需要）
+
+- 第四步：拉取用户信息(需scope为 snsapi_userinfo)
+
+
+##使用方法
+
+>例如：在 A: http://xx.cn/xxx/index.html 域名下获得微信用户的信息
+
+- 拼接 url 获得code 
+
+	http://wx.wbh5.com/auth/?url=
+  这里url 后面的地址 就是 上面的 A 地址 经过 encodeURIComponent 后，如下：
+
+	http://wx.wbh5.com/auth/?url=http%3A%2F%2Fxx.cn%2Fxxx%2Findex.html
+
+然后访问这个链接，在微信浏览器中打开，就会到授权页面
+
+- A: 域名拿到 code 之后 请求用户数据
+
+  接口地址： http://api.wbh5.com/cross/wxusers?callback=?  方式： GET
+
+```
+
+	var code = getQueryString('code');
+	
+	if (code) {
+	    Ajax({
+	        type: 'get',
+	        url: 'http://api.wbh5.com/cross/wxusers?callback=?',
+	        data: {code: code}
+	    }, function (data) {
+	        alert(JSON.stringify(data));
+	    })
+	
+	}
+
+```
---
+++ example/tool.js
@@ -0,0 +1,94 @@
+
+function getQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]);
+    return null;
+}
+
+window.storage = {
+    localStoreSupport: function() {
+        try {
+            return 'localStorage' in window && window['localStorage'] !== null;
+        } catch (e) {
+            return false;
+        }
+    },
+    set: function(name,value,days) {
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime()+(days*24*60*60*1000));
+            var expires = "; expires="+date.toGMTString();
+        }
+        else {
+            var expires = "";
+        }
+        if( this.localStoreSupport() ) {
+            localStorage.setItem(name, value);
+        }
+        else {
+            document.cookie = name+"="+value+expires+"; path=/";
+        }
+    },
+    get: function(name) {
+        if( this.localStoreSupport() ) {
+            ret = localStorage.getItem(name);
+            //console.log(typeof ret);
+            switch (ret) {
+                case 'true':
+                    return true;
+                case 'false':
+                    return false;
+                default:
+                    return ret;
+            }
+        }
+        else {
+            var nameEQ = name + "=";
+            var ca = document.cookie.split(';');
+            for(var i=0;i < ca.length;i++) {
+                var c = ca[i];
+                while (c.charAt(0)==' ') c = c.substring(1,c.length);
+                if (c.indexOf(nameEQ) == 0) {
+                    ret = c.substring(nameEQ.length,c.length);
+                    switch (ret) {
+                        case 'true':
+                            return true;
+                        case 'false':
+                            return false;
+                        default:
+                            return ret;
+                    }
+                }
+            }
+            return null;
+        }
+    },
+    del: function(name) {
+        if( this.localStoreSupport() ) {
+            localStorage.removeItem(name);
+        }
+        else {
+            this.set(name,"",-1);
+        }
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
